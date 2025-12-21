0bab264e0568781dbc996da4116710768d6407bf
huangmiao <huangmiao2468@gmail.com>
2025-12-21 15:20:47 +0800

    extract css config

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def stylesheet_link_tag_all
+    # Get all CSS files from the stylesheets directory
+    css_files = Dir.glob(Rails.root.join('app', 'assets', 'stylesheets', '*.css')).map do |file|
+      File.basename(file, '.css')
+    end
+
+    # Generate stylesheet_link_tag for each file
+    css_files.map do |file_name|
+      stylesheet_link_tag(file_name, 'data-turbo-track': 'reload')
+    end.join.html_safe
+  end
 end
