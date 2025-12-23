c5ddb8e6325a5158b7f6b0477fb8f5c29756fc40
huangmiao <huangmiao2468@gmail.com>
2025-12-23 09:35:40 +0800

    relocate PopConfirmLog

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,11 +1,9 @@
+// app/javascript/controllers/application.js
 import { Application } from "@hotwired/stimulus"
-import PopConfirmLog from "lib/confirm_log";
-
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
-window.PopConfirmLog = PopConfirmLog;
 
 export { application }
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,7 @@
+// app/javascript/controllers/index.js
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+import PopConfirmLog from "lib/confirm_log";
+window.PopConfirmLog = PopConfirmLog;
 eagerLoadControllersFrom("controllers", application)
