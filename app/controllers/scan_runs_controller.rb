9e107cda7c71f417aad2660a45814fae4539ed42
huangmiao <huangmiao2468@gmail.com>
2025-12-26 14:27:43 +0800

    add scan runs index action

--- app/controllers/scan_runs_controller.rb
+++ app/controllers/scan_runs_controller.rb
@@ -1,8 +1,25 @@
 # frozen_string_literal: true
 
 class ScanRunsController < ApplicationController
-  def index; end
-  def show; end
+  def index
+    @scan_runs =
+      ScanRun
+        .joins(repository_snapshot: :repository)
+        .joins(:lexical_pattern)
+        .left_joins(:occurrences)
+        .select(<<~SQL.squish)
+          scan_runs.*,
+          repositories.name AS repository_name,
+          repository_snapshots.commit_sha AS commit_sha,
+          lexical_patterns.name AS lexical_pattern_name,
+          COUNT(occurrences.id) AS occurrences_count
+        SQL
+        .group("scan_runs.id, repositories.name, repository_snapshots.commit_sha,  lexical_patterns.name")
+        .order("scan_runs.created_at DESC")
+  end
+
+  def destroy
+  end
 
   def create
     @repository = Repository.find_by(id: params[:repository_id])
