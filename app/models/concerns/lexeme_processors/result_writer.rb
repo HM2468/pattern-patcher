d760f858c29d467d2210f8c5e621bee03b034043
huangmiao <huangmiao2468@gmail.com>
2025-12-28 16:24:45 +0800

    fix err

--- app/models/concerns/lexeme_processors/result_writer.rb
+++ app/models/concerns/lexeme_processors/result_writer.rb
@@ -17,14 +17,14 @@ module LexemeProcessors
         }
       end
 
-      LexemeProcessResult.upsert_all(
+      ::LexemeProcessResult.upsert_all(
         rows,
         unique_by: :idx_lexeme_process_results_unique
       )
     end
 
     def write_result!(lexeme_process_job:, lexeme:, output:)
-      LexemeProcessResult.upsert(
+      ::LexemeProcessResult.upsert(
         {
           lexeme_process_job_id: lexeme_process_job.id,
           lexeme_id: lexeme.id,
