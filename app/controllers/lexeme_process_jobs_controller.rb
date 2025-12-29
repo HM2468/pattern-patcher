a0eb6c3300e14fda21a5e770530c4a89ee177c92
huangmiao <huangmiao2468@gmail.com>
2025-12-29 18:06:12 +0800

    add LexemeProcessJobsController create action

--- app/controllers/lexeme_process_jobs_controller.rb
+++ app/controllers/lexeme_process_jobs_controller.rb
@@ -1,5 +1,7 @@
 class LexemeProcessJobsController < ApplicationController
   include LexemeWorkspaceSection
+  before_action :set_lexeme_processor, only: %i[create]
+  before_action :set_lexeme_process_job, only: %i[destroy]
 
   def index
     @lexeme_process_jobs =
@@ -8,6 +10,29 @@ class LexemeProcessJobsController < ApplicationController
         .order(created_at: :desc)
   end
 
+  def create
+    @lexeme_process_job = @lexeme_processor.lexeme_process_jobs.build(status: "pending")
+    if @lexeme_process_job.save
+      flash[:success] = "Process job created successfully."
+      redirect_to lexeme_process_jobs_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def destroy
+    @lexeme_process_job.destroy!
+    flash[:success] = "Process job deleted successfully."
+    redirect_to lexeme_process_jobs_path
+  end
+
+  private
+
+  def set_lexeme_processor
+    @lexeme_processor = LexemeProcessor.find_by(id: params[:lexeme_processor_id])
+  end
+
+  def set_lexeme_process_job
+    @lexeme_process_job = LexemeProcessJob.find_by(id: params[:id])
   end
 end
