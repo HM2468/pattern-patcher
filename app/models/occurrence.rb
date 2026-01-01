02c403c6a0bc5b4c96af4aba8970ec26b0953d92
huangmiao <huangmiao2468@gmail.com>
2026-01-01 12:31:18 +0800

    use enum in occurrence model

--- app/models/occurrence.rb
+++ app/models/occurrence.rb
@@ -8,17 +8,15 @@ class Occurrence < ApplicationRecord
   has_many :replacement_actions, dependent: :destroy
   has_one :occurrence_review, dependent: :destroy
 
-  STATUSES = %w[unreviewed approved ignored replaced].freeze
-  validates :status, presence: true, inclusion: { in: STATUSES }
+  enum :status, {
+    unprocessed: "unprocessed",
+    processed:   "processed",
+    ignored:     "ignored",
+  }, default: :unprocessed
 
   validates :line_at, numericality: { only_integer: true, greater_than: 0 }
   validates :line_char_start, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
   validates :line_char_end, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
-
-  scope :unreviewed, -> { where(status: "unreviewed") }
-  scope :by_location, -> { order(file_id: :asc, line_at: :asc, line_char_start: :asc) }
-
-  before_validation :default_status, on: :create
 
   def match_range
     return nil if line_char_start.nil? || line_char_end.nil?
@@ -28,11 +26,5 @@ class Occurrence < ApplicationRecord
   def highlighted_origin_context
     highlighted = "<span class=\"ppmatchhi\">#{matched_text}</span>"
     context[0..(line_char_start - 1)] + highlighted + context[(line_char_end + 1)..]
-  end
-
-  private
-
-  def default_status
-    self.status ||= "unreviewed"
   end
 end
