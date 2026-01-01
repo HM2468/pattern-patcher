14666f2a4e9a9661e26402955d299982c5aaa48f
huangmiao <huangmiao2468@gmail.com>
2026-01-01 11:26:48 +0800

    implement localize rails generate_rendered_code

--- app/services/lexeme_processors/localize_rails.rb
+++ app/services/lexeme_processors/localize_rails.rb
@@ -9,31 +9,79 @@ module LexemeProcessors
       lex_arr.map do |item|
         lexeme_id = item.fetch(:id)
         text = item.fetch(:normalized_text).to_s
-
         simulate_external_call!
-
         locale = config.fetch("locale", "en")
         key_prefix = config.fetch("key_prefix", "gpt_trans")
-
         translated = fake_translate(text)
         i18n_key = "#{key_prefix}.#{fake_key}"
-
         {
           id: lexeme_id,
           output_json: {
             "processed_text" => translated,
             "i18n_key" => i18n_key,
-            "locale" => locale
+            "locale" => locale,
           },
           metadata: {
             "provider" => config["provider"],
-            "model" => config["model"]
-          }.compact
+            "model" => config["model"],
+          }.compact,
         }
       end
     end
 
+    def generate_rendered_code(
+      config: {},
+      lexeme_metadata: {},
+      lps_output: {},
+      file_path: ""
+    )
+      full_key = get_full_key(
+        config: config,
+        lps_output: lps_output,
+        file_path: file_path
+      )
+
+      # 处理插值参数
+      # {"interpolations" =>
+      #   {"params1" => "user_display",
+      #   "params2" => "target_user.username",
+      #   "params3" => "project.path_with_namespace",
+      #   "params4" => "UsersProject::MEMBER_NAMES[access_level]"}}
+      # 期望 interpolation 结构：
+      # {"params1"=>"user_display","params2"=>"target_user.username",...}
+      interpolation = lexeme_metadata["interpolation"]
+      params_code = ""
+      if interpolation.present?
+        pairs = interpolation.map do |k, v|
+          "#{k}: #{v}"
+        end
+        params_code = ", #{pairs.join(', ')}"
+      end
+      rendered_code = %(I18n.t("#{i18n_key}"#{params_code}))
+      metadata = { full_key: full_key}
+      [rendered_code, metadata]
+    end
+
     private
+
+    def get_full_key(
+      config: {},
+      lps_output: {},
+      file_path: ""
+    )
+      key_prefix = config.fetch("key_prefix", nil)
+      last_key = lps_output["i18n_key"]
+      pathkey_arr = file_path.to_s.split("/")
+      filename = pathkey_arr.pop
+      file_sha = ::Lexeme.sha_digest(file_path)[0..6]
+      last_key = "#{file_sha}_#{last_key}"
+      pathkey_arr.reject! { |e| e == "app" }
+      pathkey_arr << last_key
+      prefix_key = config.fetch("key_prefix", nil)
+      pathkey_arr.unshift(prefix_key) if prefix_key.present?
+      full_key = pathkey_arr.join(".")
+      full_key
+    end
 
     def simulate_external_call!
       sleep(rand(0.05..0.25))
