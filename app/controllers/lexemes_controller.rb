bbd055afc423bae80c100c455941bdbc1e347c94
huangmiao <huangmiao2468@gmail.com>
2026-01-02 14:11:22 +0800

    improve lexeme index page

--- app/controllers/lexemes_controller.rb
+++ app/controllers/lexemes_controller.rb
@@ -3,8 +3,9 @@ class LexemesController < ApplicationController
 
   def index
     @lexemes = Lexeme.order(created_at: :desc)
+      .page(params[:page])
+      .per(10)
   end
 
-  def show
-  end
+  def show; end
 end
