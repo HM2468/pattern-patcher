6f372090c2f953ddb25c59a6720e1638d6c02bcc
huangmiao <huangmiao2468@gmail.com>
2026-01-06 16:41:45 +0800

    add processor show page

--- app/models/lexeme_processor.rb
+++ app/models/lexeme_processor.rb
@@ -29,7 +29,24 @@ class LexemeProcessor < ApplicationRecord
     end
   end
 
+  def config_pretty
+    prettier_json(default_config)
+  end
+
+  def schema_pretty
+    prettier_json(output_schema)
+  end
+
   private
+
+
+  def prettier_json(value)
+    return "" if value.nil?
+
+    JSON.pretty_generate(value.as_json)
+  rescue JSON::GeneratorError, TypeError
+    value.to_json
+  end
 
   def will_enable?
     enabled? && (new_record? || will_save_change_to_enabled?)
