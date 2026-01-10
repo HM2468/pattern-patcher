f7cf3731406466105f160a536225402f6a14e0cb
huangmiao <huangmiao2468@gmail.com>
2026-01-10 23:26:36 +0800

    implement occurrence show page

--- app/controllers/occurrences_controller.rb
+++ app/controllers/occurrences_controller.rb
@@ -26,6 +26,33 @@ class OccurrencesController < ApplicationController
   end
 
   def show
+    @occurrence = Occurrence.find(params[:id])
+    @file = @occurrence.repository_file
+    @repo = @file.repository
+
+    raw_content = @repo.git_cli.read_file(@file.blob_sha).to_s
+    raw_lines = raw_content.lines.map { |l| l.chomp("\n").chomp("\r") }
+
+    idx = [@occurrence.line_at.to_i - 1, 0].max
+    idx = [idx, raw_lines.length - 1].min if raw_lines.any?
+    old_line_from_blob = raw_lines[idx].to_s
+
+    old_line_highlighted =
+      if @occurrence.line_char_start && @occurrence.line_char_end
+        @occurrence.context = old_line_from_blob if @occurrence.respond_to?(:context=)
+        @occurrence.highlighted_deletion.to_s
+      else
+        ERB::Util.html_escape(old_line_from_blob)
+      end
+
+    @diff = GithubLikeDiff.new(
+      path: @file.path,
+      raw_lines: raw_lines,
+      target_lineno: @occurrence.line_at,
+      old_line_override: old_line_highlighted,
+      new_line: nil,
+      context_lines: 3
+    )
   end
 
   private
